1102b11e0d0a81c8a6e9bbdd8f0d3c9d23df8a47
MelNetProblem <82233804+MelNetProblem@users.noreply.github.com>
2021-04-17 12:21:17 +0300

    Add files via upload

--- Ultimate_pong.py
+++ Ultimate_pong.py
@@ -1,10 +1,12 @@
 from pygame import *
 window=display.set_mode((700,500))
 window.fill((9,1,3))
+display.set_caption("Ultimate Pong")
 fps = 144
 clock=time.Clock()
 game= True
-
+speed_x = 4
+speed_y = 4
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image,player_x,player_y, size_x, size_y, player_speed):
         super().__init__()
@@ -19,15 +21,15 @@ class Player(GameSprite):
     def update_l(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y >5:
-            self.rect.y -= speed
+            self.rect.y -= self.speed
         if keys_pressed[K_s] and self.rect.y<405:
-            self.rect.y += speed 
+            self.rect.y += self.speed 
     def update_r(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y >5:
-            self.rect.y -= speed
+            self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y<405:
-            self.rect.y += speed 
+            self.rect.y += self.speed 
 class Enemy(GameSprite):
     def update(self):
         global lose
@@ -36,19 +38,32 @@ class Enemy(GameSprite):
             self.rect.y = 0
             self.rect.x = randint(100,600)
             lose+=1
+platform_l= Player("dsad.png",20,200,70,70,10)
+platform_r= Player("tako.png",610,200,70,70,10)
+ball = GameSprite("balls.png",250,300,35,35,0)
 
+font1 = font.Font
 #aggy birsds
 
 
 while game:
+    window.fill((9,1,3))
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if ball.rect.y > 470 or ball.rect.y < 10:
+        speed_y *= -1
+    if sprite.collide_rect(platform_l,ball) or sprite.collide_rect(platform_r,ball):
+        speed_x *= -1
     clock.tick(fps)
-    display.update()
-
-
-
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+    platform_l.reset()
+    platform_r.reset()
+    ball.reset()
+    platform_l.update_l()
+    platform_r.update_r()
+    display.update()    
 
 
 
